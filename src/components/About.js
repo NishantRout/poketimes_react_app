252c8ae0e1d25a507f20df926cf57a495b444d1c
NishantRout <48598347+NishantRout@users.noreply.github.com>
2020-08-12 17:37:54 +0530

    edits-5:37pm

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt in saepe illo voluptas error ab itaque eligendi nobis magni vero libero atque delectus, quasi quis debitis fugiat adipisci dolores?</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, nesciunt in saepe illo voluptas error ab itaque eligendi nobis magni vero libero atque delectus, quasi quis debitis fugiat adipisci dolores?</p>
+        </div>
+    );
+}
+
+export default Home;
